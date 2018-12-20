314d5085663e31429a0f9bd23e81e40cfc5fc3c2
Unknown <big50201@gmail.com>
2018-12-20 16:28:04 +0800

    prototype

--- react-slack-clone/src/components/App.js
+++ react-slack-clone/src/components/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import {Grid} from 'semantic-ui-react';
 import './App.css';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      app
-      </div>
+      <Grid columns="equal" className="app" style={{backgroundColor:'#eee'}}>
+        <ColorPanel/>
+        <SidePanel/>
+        <Grid.Column style={{marginLeft:320}}>
+          <Messages/>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <MetaPanel/>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
