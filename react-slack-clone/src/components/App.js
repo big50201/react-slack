f14a86964c280fc68bd9710668f5a24bc56cb192
jennifer.sung <jennifer.sung@skylinetw.com>
2018-12-28 17:37:30 +0800

    firebase add user color

--- react-slack-clone/src/components/App.js
+++ react-slack-clone/src/components/App.js
@@ -11,7 +11,9 @@ class App extends Component {
     const {currentUser,currentChannel,isPrivateChannel,userPosts} = this.props;
     return (
       <Grid columns="equal" className="app" style={{backgroundColor:'#eee'}}>
-        <ColorPanel/>
+        <ColorPanel 
+        key={currentUser && currentUser.name}
+        currentUser={currentUser}/>
         <SidePanel 
           key={currentUser && currentUser.uid}
           currentUser={currentUser}/>
@@ -24,7 +26,7 @@ class App extends Component {
         </Grid.Column>
         <Grid.Column width={4}>
           <MetaPanel 
-            key={currentChannel && currentChannel.id}
+            key={currentChannel && currentChannel.name}
             currentChannel = {currentChannel}
             isPrivateChannel={isPrivateChannel}
             userPosts={userPosts}
--- react-slack-clone/src/components/ColorPanel/ColorPanel.js
+++ react-slack-clone/src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
-import {Sidebar,Menu,Divider,Button} from 'semantic-ui-react';
+import {Sidebar,Menu,Divider,Button,Modal,Icon,Label,Segment} from 'semantic-ui-react';
+import {SliderPicker} from 'react-color';
+import firebase from '../../firebase';
+
 class ColorPanel extends Component {
+    state = {
+        modal:false,
+        primary:'',
+        secondary:'',
+        user:this.props.currentUser,
+        userRef:firebase.database().ref('users')
+    }
+
+    openModal =()=>this.setState({modal:true});
+    closeModal =()=>this.setState({modal:false});
+    handleChangePrimary=color=>this.setState({primary:color.hex});
+    handleChangeSecondary= color=>this.setState({secondary:color.hex});
+    handleSaveColor = ()=>{
+        if(this.state.primary && this.state.secondary){
+            this.saveColors(this.state.primary,this.state.secondary);
+        }
+    }
+
+    saveColors = (primary,secondary)=>{
+        this.state.userRef.child(`${this.state.user.uid}/colors`)
+        .push()
+        .update({
+            primary,
+            secondary
+        })
+        .then(()=>{
+            console.log('color added');
+            this.closeModal();
+        })
+        .catch(err=>console.log(err));
+    }
     render() {
+        const {modal,primary,secondary} = this.state;
         return (
             <Sidebar
                 as={Menu}
@@ -12,7 +47,29 @@ class ColorPanel extends Component {
                 width="very thin"
             >
                 <Divider/>
-                <Button icon="add" color="blue" size="small"/>
+                <Button icon="add" color="blue" size="small" onClick={this.openModal}/>
+                <Modal open={modal} onClose={this.closeModal}>
+                    <Modal.Header>Choose App Colors</Modal.Header>
+                    <Modal.Content>
+                        <Segment inverted>
+                            <Label content="Primary color"/>
+                            <SliderPicker color={primary} onChange={this.handleChangePrimary}/>
+                        </Segment>
+                        <Segment inverted>
+                            <Label content="Secondary color"/>
+                            <SliderPicker color={secondary} onChange={this.handleChangeSecondary}/>
+                        </Segment>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="green" inverted onClick={this.handleSaveColor}>
+                            <Icon name="checkmark"/> Save Colors
+                        </Button>
+                        <Button color="red" inverted onClick={this.closeModal}>
+                            <Icon name="remove"/> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+               
             </Sidebar>
         );
     }
