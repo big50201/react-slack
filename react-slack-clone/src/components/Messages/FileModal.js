f4e8d0283a019b997a43ecd99a18374fa2bd9924
jennifer.sung <jennifer.sung@skylinetw.com>
2019-11-22 14:20:19 +0800

    file button edit

--- react-slack-clone/src/components/Messages/FileModal.js
+++ react-slack-clone/src/components/Messages/FileModal.js
@@ -1,72 +1,76 @@
-import React, { Component } from 'react';
-import {Modal,Input,Icon,Button} from 'semantic-ui-react';
-import mime from 'mime-types';
+import React, { Component } from "react";
+import { Modal, Input, Icon, Button } from "semantic-ui-react";
+import mime from "mime-types";
 
 class FileModal extends Component {
-    state = {
-        file:null,
-        authorized:['image/png','image/jpeg']
+  state = {
+    file: null,
+    authorized: ["image/png", "image/jpeg"]
+  };
+
+  fileInputRef = React.createRef();
+
+  addFile = e => {
+    const file = e.target.files[0];
+    if (file) {
+      this.setState({ file });
     }
 
-    addFile = e=>{
-        const file = e.target.files[0];
-        if(file){
-            this.setState({file});
-        }
+    console.log(file);
+  };
+
+  sendFile = () => {
+    const { file } = this.state;
+    const { uploadFile, closeModal } = this.props;
+    if (file !== null) {
+      if (this.isAuthorized(file.name)) {
+        //send file
+        const metadata = { contentType: mime.lookup(file.name) };
+        uploadFile(file, metadata);
+        closeModal();
+        this.clearFile();
+      }
     }
+  };
 
-    sendFile = ()=>{
-        const {file} = this.state;
-        const {uploadFile,closeModal} = this.props;
-        if(file!==null){
-            if(this.isAuthorized(file.name)){
-                //send file
-                const metadata = {contentType:mime.lookup(file.name)};
-                uploadFile(file,metadata);
-                closeModal();
-                this.clearFile();
-            }
-        }
-        
-    }
+  isAuthorized = filename =>
+    this.state.authorized.includes(mime.lookup(filename));
 
-    isAuthorized = filename=>this.state.authorized.includes(mime.lookup(filename));
+  clearFile = () => this.setState({ file: null });
 
-    clearFile = ()=>this.setState({file:null});
-
-    render() {
-        const {modal,closeModal} = this.props 
-        return (
-            <Modal open={modal} onClose={closeModal}>
-                <Modal.Header>Select an Image File</Modal.Header>
-                <Modal.Content>
-                    <Input 
-                    fluid={true}
-                    label="File types: jpg,png"
-                    name="file"
-                    type="file"
-                    onChange={this.addFile}
-                    />
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button
-                        color="green"
-                        inverted
-                        onClick={this.sendFile}
-                    >
-                        <Icon name="checkmark"/>Send
-                    </Button>
-                    <Button
-                        color="red"
-                        inverted
-                        onClick={closeModal}
-                    >
-                        <Icon name="remove"/>Cancel
-                    </Button>
-                </Modal.Actions>
-            </Modal>
-        );
-    }
+  render() {
+    const { modal, closeModal } = this.props;
+    return (
+      <Modal open={modal} onClose={closeModal}>
+        <Modal.Header>Select an Image File</Modal.Header>
+        <Modal.Content>
+          <Button
+            content="Choose File"
+            labelPosition="left"
+            icon="file"
+            onClick={() => this.fileInputRef.current.click()}
+          />
+          <input
+            ref={this.fileInputRef}
+            type="file"
+            hidden
+            onChange={this.addFile}
+          />
+          {this.state.file && this.state.file.name}
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="green" inverted onClick={this.sendFile}>
+            <Icon name="checkmark" />
+            Send
+          </Button>
+          <Button color="red" inverted onClick={closeModal}>
+            <Icon name="remove" />
+            Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
+  }
 }
 
 export default FileModal;
--- react-slack-clone/src/index.js
+++ react-slack-clone/src/index.js
@@ -1,62 +1,71 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-import {BrowserRouter as Router,Switch,Route,withRouter} from 'react-router-dom';
-import Register from './components/Auth/Register';
-import Login from './components/Auth/Login';
-import 'semantic-ui-css/semantic.min.css';
-import firebase from 'firebase';
-import {createStore,applyMiddleware} from 'redux';
-import {Provider,connect} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import root_reducer from './reducers';
-import {setUser,clearUser} from './actions';
-import Spinner from './Spinner';
-import thunk from 'redux-thunk';
-import {createLogger} from 'redux-logger' // 利用redux-logger記錄日誌
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import "semantic-ui-css/semantic.min.css";
+import firebase from "firebase";
+import { createStore, applyMiddleware } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import root_reducer from "./reducers";
+import { setUser, clearUser } from "./actions";
+import Spinner from "./Spinner";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger"; // 利用redux-logger記錄日誌
 // 調用日誌 collapsed讓action折疊
-const loggerMiddleware = createLogger({collapsed:true});
+const loggerMiddleware = createLogger({ collapsed: true });
 const middleware = [thunk, loggerMiddleware];
 const store = createStore(
-    root_reducer,
-    composeWithDevTools(
-        applyMiddleware(...middleware)
-    )
+  root_reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
-class Root extends Component{
-    componentDidMount(){
-        firebase.auth().onAuthStateChanged(user=>{
-            if(user){
-                this.props.setUser(user);
-                this.props.history.push('/');
-            }else{
-                this.props.history.push('/login');
-                this.props.clearUser();
-            }
-        })
-    }
-    render(){
-        return this.props.isLoading ? <Spinner/> : (
-            <Switch>
-                <Route exact path="/" component={App}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/register" component={Register}/>
-            </Switch>);
-    }
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser();
+      }
+    });
+  }
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
 }
-const mapStateFromProps = state=>({
-    isLoading:state.user.isLoading
-})
-const RootWithAuth = withRouter(connect((mapStateFromProps),{setUser,clearUser})(Root));
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+const RootWithAuth = withRouter(
+  connect(mapStateFromProps, { setUser, clearUser })(Root)
+);
 ReactDOM.render(
-<Provider store={store}>
+  <Provider store={store}>
     <Router>
-        <RootWithAuth />
-    </Router> 
-</Provider>
-, document.getElementById('root'));
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
